3240450d36df37d1ee03a3ea4d1d2323e77ea151
Claire.b <claire.bourdale@gmail.com>
2016-01-27 17:19:03 +0100

    Index.php : mise en place de la trame html

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Claire Bourdalé</title>
+<meta charset="utf-8" />
+<meta name="description" content="Claire Bourdalé, développement web freelance sur Caen en Normandie" />
+<meta name="viewport" content="width=device-width" />
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+
+</head>
+<body>
+
+</body>
+
+
+</html>
