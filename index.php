3fb6ea7c0eb15fb72a5a7d328177b99a0f64042a
Claire.b <claire.bourdale@gmail.com>
2016-01-27 19:02:07 +0100

    index.php : ajout des liens vers js et bootstrap

--- index.php
+++ index.php
@@ -5,12 +5,23 @@
 <meta charset="utf-8" />
 <meta name="description" content="Claire Bourdalé, développement web freelance sur Caen en Normandie" />
 <meta name="viewport" content="width=device-width" />
-<link rel="stylesheet" type="text/css" href="css/styles.css">
+<link rel="stylesheet" type="text/css" href="css/design.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
 </head>
+
 <body>
 
+
+<!-- xxxxxxxxxxxxxxxxx Includes js xxxxxxxxxxxxxxxxxxxx -->	
+<script src="js/jquery.js"></script>
+<script src="js/jquery.js"></script>
 </body>
-
-
 </html>
