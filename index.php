42df45bac3aebf64406ef6d487aca24e4adf5e49
Claire.b <claire.bourdale@gmail.com>
2016-01-28 17:54:58 +0100

    index.php : mise en place du squelette daccueil

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 <meta name="viewport" content="width=device-width; initial-scale=1.0; " />
 <link rel="stylesheet" type="text/css" href="css/design.css">
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Kalam:700' rel='stylesheet' type='text/css'>
 
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -26,7 +28,7 @@
 		<a class="icone-menu-mobile" id="icone-menu" href="#"></a>
 
 		<div class="header-centre">
-			<div class="logo"><a href="#">Claire Bourdalé</a></div>
+			<div class="logo"><a href="index.php">Claire Bourdalé</a></div>
 			<nav class="menu">
 					<ul>
 						<li><a href="#">Profil</a></li>
@@ -41,29 +43,132 @@
 
 
 	<div class="page-contenu">
-		<h1>Claire Bourdalé</h1>
+		<div class="container">
+			
+			<div class="col-xs-12">
+				<h1>Développement Web et gestion de projets sportifs</h1>
+				<blockquote>Mox dicta finierat, multitudo omnis ad, quae imperator voluit, promptior laudato consilio consensit in pacem ea ratione maxime percita, quod norat expeditionibus crebris fortunam eius in malis tantum civilibus vigilasse, cum autem bella moverentur externa, accidisse plerumque luctuosa, icto post haec foedere gentium ritu perfectaque sollemnitate imperator Mediolanum ad hiberna discessit</blockquote>
+			</div>
 
-		<p>Mox dicta finierat, multitudo omnis ad, quae imperator voluit, promptior laudato consilio consensit in pacem ea ratione maxime percita, quod norat expeditionibus crebris fortunam eius in malis tantum civilibus vigilasse, cum autem bella moverentur externa, accidisse plerumque luctuosa, icto post haec foedere gentium ritu perfectaque sollemnitate imperator Mediolanum ad hiberna discessit.</p>
 
-		<p>Eminuit autem inter humilia supergressa iam impotentia fines mediocrium delictorum nefanda Clematii cuiusdam Alexandrini nobilis mors repentina; cuius socrus cum misceri sibi generum, flagrans eius amore, non impetraret, ut ferebatur, per palatii pseudothyrum introducta, oblato pretioso reginae monili id adsecuta est, ut ad Honoratum tum comitem orientis formula missa letali omnino scelere nullo contactus idem Clematius nec hiscere nec loqui permissus occideretur.</p>
+			<div class="col-xs-12">
+				<h2>Profil compétences</h2>
+			</div>	
 
-		<p>Itaque tum Scaevola cum in eam ipsam mentionem incidisset, exposuit nobis sermonem Laeli de amicitia habitum ab illo secum et cum altero genero, C. Fannio Marci filio, paucis diebus post mortem Africani. Eius disputationis sententias memoriae mandavi, quas hoc libro exposui arbitratu meo; quasi enim ipsos induxi loquentes, ne 'inquam' et 'inquit' saepius interponeretur, atque ut tamquam a praesentibus coram haberi sermo videretur.</p>
+			<div class="row">
 
-		<p>Excitavit hic ardor milites per municipia plurima, quae isdem conterminant, dispositos et castella, sed quisque serpentes latius pro viribus repellere moliens, nunc globis confertos, aliquotiens et dispersos multitudine superabatur ingenti, quae nata et educata inter editos recurvosque ambitus montium eos ut loca plana persultat et mollia, missilibus obvios eminus lacessens et ululatu truci perterrens.</p>
+				<div class="col-lg-4 col-xs-12"> <!-- Bloc développement web -->
+					<div class="col-lg-12 col-xs-4">
+						<a href="#" class="thumbnail"><img src="img/web.jpg" alt="Développement web" title="" /></a>
+					</div>
+					<div class="col-lg-12 col-xs-8">
+						<h3>Développement web</h3>
+						<p>Création, développement et référencement de sites Internet responsives</p>
+					</div>
+				</div>	<!-- Fin bloc développement web -->
 
-		<p>Proinde concepta rabie saeviore, quam desperatio incendebat et fames, amplificatis viribus ardore incohibili in excidium urbium matris Seleuciae efferebantur, quam comes tuebatur Castricius tresque legiones bellicis sudoribus induratae.</p>
+				<div class="col-lg-4 col-xs-12"> <!-- Bloc gestion administrative -->
+					<div class="col-lg-12 col-xs-4">
+						<a href="#" class="thumbnail"><img src="img/gestion.jpg" alt="Gestion administrative, comptable et commerciale" title="" /></a>
+					</div>
+					<div class="col-lg-12 col-xs-8">
+						<h3>Gestion de structures sportives</h3>
+						<p>Gestion administrative, comptable et commerciale de structures sportives</p>
+					</div>
+				</div><!-- Fin bloc gestion administrative -->
 
-		<p>Mox dicta finierat, multitudo omnis ad, quae imperator voluit, promptior laudato consilio consensit in pacem ea ratione maxime percita, quod norat expeditionibus crebris fortunam eius in malis tantum civilibus vigilasse, cum autem bella moverentur externa, accidisse plerumque luctuosa, icto post haec foedere gentium ritu perfectaque sollemnitate imperator Mediolanum ad hiberna discessit.</p>
+				<div class="col-lg-4 col-xs-12"> <!-- Bloc organisation événementielle -->
+					<div class="col-lg-12 col-xs-4">
+						<a href="#" class="thumbnail"><img src="img/evenementiel.jpg" alt="Organisation d'événementiels sportifs" title="" /></a>
+					</div>
+					<div class="col-lg-12 col-xs-8">
+						<h3>Gestion de projets événementiels</h3>
+						<p>Organisation de manifestations sportives de sa définition à sa conceptualisation</p>
+					</div>
+				</div>	<!--Fin bloc organisation événementielle -->
 
-		<p>Eminuit autem inter humilia supergressa iam impotentia fines mediocrium delictorum nefanda Clematii cuiusdam Alexandrini nobilis mors repentina; cuius socrus cum misceri sibi generum, flagrans eius amore, non impetraret, ut ferebatur, per palatii pseudothyrum introducta, oblato pretioso reginae monili id adsecuta est, ut ad Honoratum tum comitem orientis formula missa letali omnino scelere nullo contactus idem Clematius nec hiscere nec loqui permissus occideretur.</p>
+			</div><!-- fin du row profil compétences -->
 
-		<p>Itaque tum Scaevola cum in eam ipsam mentionem incidisset, exposuit nobis sermonem Laeli de amicitia habitum ab illo secum et cum altero genero, C. Fannio Marci filio, paucis diebus post mortem Africani. Eius disputationis sententias memoriae mandavi, quas hoc libro exposui arbitratu meo; quasi enim ipsos induxi loquentes, ne 'inquam' et 'inquit' saepius interponeretur, atque ut tamquam a praesentibus coram haberi sermo videretur.</p>
+			<div class="col-xs-12">
+				<h2>Outils &amp; techniques</h2>
 
-		<p>Excitavit hic ardor milites per municipia plurima, quae isdem conterminant, dispositos et castella, sed quisque serpentes latius pro viribus repellere moliens, nunc globis confertos, aliquotiens et dispersos multitudine superabatur ingenti, quae nata et educata inter editos recurvosque ambitus montium eos ut loca plana persultat et mollia, missilibus obvios eminus lacessens et ululatu truci perterrens.</p>
+				<div class="row">
+					<div class="col-lg-2 col-md-3 col-xs-6">
+					<p><img src="img/css.png" alt="CSS3" title="" /><span class="outil">CSS3</span></p>
+					</div>
 
-		<p>Proinde concepta rabie saeviore, quam desperatio incendebat et fames, amplificatis viribus ardore incohibili in excidium urbium matris Seleuciae efferebantur, quam comes tuebatur Castricius tresque legiones bellicis sudoribus induratae.</p>
+					<div class="col-lg-2 col-md-3 col-xs-6">
+					<p><img src="img/html5.png" alt="HTML5" title="" /><span class="outil">HTML5</span></p>
+					</div>
 
-	</div>
+					<div class="col-lg-2 col-md-3 col-xs-6">
+					<p><img src="img/php.png" alt="PHP" title="" /><span class="outil">PHP</span></p>
+					</div>
+
+					<div class="col-lg-2 col-md-3 col-xs-6">
+					<p><img src="img/mysql.png" alt="MySql" title="" /><span class="outil">MySql</span></p>
+					</div>
+
+					<div class="col-lg-2 col-md-3 col-xs-6">
+					<p><img src="img/wordpress.png" alt="Wordpress" title="" /><span class="outil">CMS Wordpress</span></p>
+					</div>
+
+					<div class="col-lg-2 col-md-3 col-xs-6">
+					<p><img src="img/dotclear.jpg" alt="Dotclear" title="" /><span class="outil">CMS Dotclear</span></p>
+					</div>
+
+					<div class="col-lg-2 col-md-3 col-xs-6">
+					<p><img src="img/gimp.png" alt="" title="" /><span class="outil">Gimp</span></p>
+					</div>
+
+					<div class="col-lg-2 col-md-3 col-xs-6">
+					<p><img src="img/photoshop.jpg" alt="" title="" /><span class="outil">Photoshop</span></p>
+					</div>
+
+					<div class="col-lg-2 col-md-3 col-xs-6">
+					<p><img src="img/inkscape.png" alt="" title="" /><span class="outil">Inkscape</span></p>
+					</div>
+
+					<div class="col-lg-2 col-md-3 col-xs-6">
+					<p><img src="img/css.png" alt="" title="" /> CSS</p>
+					</div>
+
+					<div class="col-lg-2 col-md-3 col-xs-6">
+					<p><img src="img/css.png" alt="" title="" /> CSS</p>
+					</div>
+
+					<div class="col-lg-2 col-md-3 col-xs-6">
+					<p><img src="img/css.png" alt="" title="" /> CSS</p>
+					</div>
+
+
+				</div>
+			</div>
+
+
+
+			<p>Mox dicta finierat, multitudo omnis ad, quae imperator voluit, promptior laudato consilio consensit in pacem ea ratione maxime percita, quod norat expeditionibus crebris fortunam eius in malis tantum civilibus vigilasse, cum autem bella moverentur externa, accidisse plerumque luctuosa, icto post haec foedere gentium ritu perfectaque sollemnitate imperator Mediolanum ad hiberna discessit.</p>
+
+			<p>Eminuit autem inter humilia supergressa iam impotentia fines mediocrium delictorum nefanda Clematii cuiusdam Alexandrini nobilis mors repentina; cuius socrus cum misceri sibi generum, flagrans eius amore, non impetraret, ut ferebatur, per palatii pseudothyrum introducta, oblato pretioso reginae monili id adsecuta est, ut ad Honoratum tum comitem orientis formula missa letali omnino scelere nullo contactus idem Clematius nec hiscere nec loqui permissus occideretur.</p>
+
+			<p>Itaque tum Scaevola cum in eam ipsam mentionem incidisset, exposuit nobis sermonem Laeli de amicitia habitum ab illo secum et cum altero genero, C. Fannio Marci filio, paucis diebus post mortem Africani. Eius disputationis sententias memoriae mandavi, quas hoc libro exposui arbitratu meo; quasi enim ipsos induxi loquentes, ne 'inquam' et 'inquit' saepius interponeretur, atque ut tamquam a praesentibus coram haberi sermo videretur.</p>
+
+			<p>Excitavit hic ardor milites per municipia plurima, quae isdem conterminant, dispositos et castella, sed quisque serpentes latius pro viribus repellere moliens, nunc globis confertos, aliquotiens et dispersos multitudine superabatur ingenti, quae nata et educata inter editos recurvosque ambitus montium eos ut loca plana persultat et mollia, missilibus obvios eminus lacessens et ululatu truci perterrens.</p>
+
+			<p>Proinde concepta rabie saeviore, quam desperatio incendebat et fames, amplificatis viribus ardore incohibili in excidium urbium matris Seleuciae efferebantur, quam comes tuebatur Castricius tresque legiones bellicis sudoribus induratae.</p>
+
+			<p>Mox dicta finierat, multitudo omnis ad, quae imperator voluit, promptior laudato consilio consensit in pacem ea ratione maxime percita, quod norat expeditionibus crebris fortunam eius in malis tantum civilibus vigilasse, cum autem bella moverentur externa, accidisse plerumque luctuosa, icto post haec foedere gentium ritu perfectaque sollemnitate imperator Mediolanum ad hiberna discessit.</p>
+
+			<p>Eminuit autem inter humilia supergressa iam impotentia fines mediocrium delictorum nefanda Clematii cuiusdam Alexandrini nobilis mors repentina; cuius socrus cum misceri sibi generum, flagrans eius amore, non impetraret, ut ferebatur, per palatii pseudothyrum introducta, oblato pretioso reginae monili id adsecuta est, ut ad Honoratum tum comitem orientis formula missa letali omnino scelere nullo contactus idem Clematius nec hiscere nec loqui permissus occideretur.</p>
+
+			<p>Itaque tum Scaevola cum in eam ipsam mentionem incidisset, exposuit nobis sermonem Laeli de amicitia habitum ab illo secum et cum altero genero, C. Fannio Marci filio, paucis diebus post mortem Africani. Eius disputationis sententias memoriae mandavi, quas hoc libro exposui arbitratu meo; quasi enim ipsos induxi loquentes, ne 'inquam' et 'inquit' saepius interponeretur, atque ut tamquam a praesentibus coram haberi sermo videretur.</p>
+
+			<p>Excitavit hic ardor milites per municipia plurima, quae isdem conterminant, dispositos et castella, sed quisque serpentes latius pro viribus repellere moliens, nunc globis confertos, aliquotiens et dispersos multitudine superabatur ingenti, quae nata et educata inter editos recurvosque ambitus montium eos ut loca plana persultat et mollia, missilibus obvios eminus lacessens et ululatu truci perterrens.</p>
+
+			<p>Proinde concepta rabie saeviore, quam desperatio incendebat et fames, amplificatis viribus ardore incohibili in excidium urbium matris Seleuciae efferebantur, quam comes tuebatur Castricius tresque legiones bellicis sudoribus induratae.</p>
+		</div>
+
+	</div><!-- fin du page contenu -->
 	<div class="site-cache" id="site-cache"></div><!-- fin du site-cache -->
 
 
