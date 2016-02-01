3e363445230c3523622ee104ae3706559a0ff5fc
Claire.b <claire.bourdale@gmail.com>
2016-02-01 16:31:22 +0100

    index.php : ajout des incs et des citations

--- index.php
+++ index.php
@@ -2,50 +2,11 @@
 <html>
 <head>
 <title>Claire Bourdalé, développement et intrégation web freelance, gestion de projets sportifs | Caen - Normandie</title>
-<meta charset="utf-8" />
 <meta name="description" content="Claire Bourdalé, développement et intrégation web freelance, gestion de projets sportifs | Caen - Normandie" />
-<meta name="viewport" content="width=device-width; initial-scale=1.0; " />
-<link rel="stylesheet" type="text/css" href="css/design.css">
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-<link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
-<link href='https://fonts.googleapis.com/css?family=Kalam:700' rel='stylesheet' type='text/css'>
 
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-<div class="site-container">	
-<div class="site-pusher">
+<?php include('inc/head.php'); ?>
 	
-	<header>
-
-		<a class="icone-menu-mobile" id="icone-menu" href="#"></a>
-
-		<div class="header-centre">
-			<div class="logo"><a href="index.php">Claire Bourdalé</a></div>
-			<nav class="menu">
-					<ul>
-						<li><a href="index.php">Profil</a></li>
-						<li class="barre">|</li>
-						<li><a href="#">Parcours</a></li>
-						<li class="barre">|</li>
-						<li><a href="#">Book</a></li>
-						<li class="barre">|</li>
-						<li><a href="#">A propos</a></li>
-						<li class="barre">|</li>
-						<li><a href="#">Contact</a></li>
-						<li><a href="http://www.viadeo.com/profile/00217sadl5wa4zpd" target="_blank"><img src="img/viadeo.png" alt="Viadéo" title="" /></a></li>
-						<li><a href="" target="_blank"><img src="img/linked.png" alt="Linked In" title="" /></a></li>
-					</ul>
-			</nav>
-		</div>
-	</header>
+<?php include('inc/menu.php'); ?>
 
 
 	<div class="page-contenu"><!-- Tout le contenu en-dessous du header -->
@@ -174,41 +135,4 @@
 
 		</div><!-- fin du container -->
 
-		<footer class="container">
-			<div class="row">
-				<div class="col-xs-5">
-					<blockquote>
-						Un bon site Web est toujours “en construction” !
-						<small class="pull-right">Anonyme</small>
-					</blockquote>
-				</div>
-
-
-				<div class="col-xs-offset-1 col-xs-6">
-					<div class="panel panel-default">
-		  				<div class="panel-heading">
-		    				<h4 class="panel-title">Claire Bourdalé</h4>
-		  				</div>
-		  				<div class="panel-body">
-		   					 <p>Retrouvez mon profil</p>	
-		   					 <p><a href="http://www.viadeo.com/profile/00217sadl5wa4zpd" target="_blank"><img src="img/viadeo.png" alt="Viadéo"/> Viadéo</a></p>
-		   					 <p><a href="#" target="_blank"><img src="img/linked.png" alt="Linked In"/> Linked In</a></p>
-		   					 
-		  				</div>
-					</div>
-				</div>
-			</div>
-		</footer>
-
-	</div><!-- fin du page contenu -->
-	<div class="site-cache" id="site-cache"></div><!-- fin du site-cache (pour la partie ombrée cliquable sur mobile) -->
-
-
-</div><!-- fin du site pusher (pour le décalage sur mobile) -->
-</div><!-- fin du site container -->
-
-<!-- xxxxxxxxxxxxxxxxx Includes js xxxxxxxxxxxxxxxxxxxx -->	
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
-<script type="text/javascript" src="js/menu.js"></script>
-</body>
-</html>
+<?php include('inc/footer.php'); ?>
