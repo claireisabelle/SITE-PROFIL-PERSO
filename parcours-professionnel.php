7cc677fd937199ba44ae1a912fdc9aaba7d6eaa0
Claire.b <claire.bourdale@gmail.com>
2016-02-01 19:35:28 +0100

    création de la page parcours

---
+++ parcours-professionnel.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Parcours : étapes professionnelles de la gestion du sport à la création de site Internet</title>
+<meta name="description" content="Formation universitaire en Management du Sport, parcours en communication, organisation événementielle, développement web" />
+
+<?php include('inc/head.php'); ?>
+	
+<?php include('inc/menu.php'); ?>
+
+
+	<div class="page-contenu"><!-- Tout le contenu en-dessous du header -->
+		<div class="container">
+			
+			<div class="col-xs-12">
+				<h1>Parcours professionnel</h1>
+				<blockquote>Capacité d'adaptation, polyvalence, ouverture d'esprit, curiosité, bonne humeur : des qualificatifs qui me définissent dans les missions qui me sont confiées.</blockquote>
+			</div>
+
+
+			<div class="col-xs-12"><!-- Rubrique formation -->
+				<h2>Formation</h2>
+				<ul>
+					<li><strong>2005 au <?php afficherDate(); ?> :</strong> Formation autodidacte en développement web</li>
+				</ul>
+
+				<ul>
+					<li><strong>2004 :</strong> Master / DESS Management des Organisations Sportives (Université de Caen)</li>
+					<li><strong>2003 :</strong> Maîtrise Management du Sport (Université de Caen)</li>
+					<li><strong>2002 :</strong> Licence Management du Sport (Université de Caen)</li>
+					<li><strong>2001 :</strong> DEUG STAPS (Université de Caen)</li>
+					<li><strong>1999 :</strong> Baccalauréat Scientifique (Granville)</li>
+				</ul>
+			</div><!-- Fin rubrique formation -->
+
+			<div class="col-xs-12">
+				<h2>Etapes professionnelles</h2>
+			</div>
+
+			<div class="row"><!-- Row ARROWEB -->
+				<div class="col-xs-2">
+					<p class="thumbnail"><img src="img/logo-arroweb.png" alt="Arroweb, conception de site Internet" title="" /></p>
+				</div>
+				<div class="col-xs-10">
+					<h3>Arroweb (<em>depuis 2009</em>)</h3>
+					<h4>Webmaster Freelance</h4>
+					<p>Conception d'une soixantaine de sites Internet </p>
+				</div>
+			</div><!-- Fin du Row ARROWEB -->
+
+
+			<div class="row"><!-- Row EOLIA NORMANDIE -->
+				<div class="col-xs-2">
+					<p class="thumbnail"><img src="img/logo-eolia.jpg" alt="Eolia Normandie" title="" /></p>
+				</div>
+				<div class="col-xs-10">
+					<h3>Eolia Normandie (<em>2013 - 2015</em>)</h3>
+					<h4>Agent de gestion et de développement</h4>
+					<p>Organisation événementielle, gestion administrative &amp; commerciale de la base nautique, gestion de la vie associative, communication web &amp; print, relation medias, comptabilité </p>
+				</div>
+			</div><!-- Fin du Row EOLIA NORMANDIE -->		
+
+
+			<div class="row"><!-- Row ARROSPORT -->
+				<div class="col-xs-2">
+					<p class="thumbnail"><img src="img/logo-arrosport.png" alt="Arrosport, gestion de projets sportifs" title="" /></p>
+				</div>
+				<div class="col-xs-10">
+					<h3>Arrosport (<em>2010 - 2013</em>)</h3>
+					<h4>Chargée de projets sportifs Freelance</h4>
+					<p>Organisation des championnats de France de char à voile 2010 &amp; 2011, organisation de courses à pied, réalisation du plan de développement de la ligue de Normandie de char à voile</p>
+				</div>
+			</div><!-- Fin du Row ARROWEB -->
+
+
+			<div class="row"><!-- Row IPROCORPORATE -->
+				<div class="col-xs-2">
+					<p class="thumbnail"><img src="img/logo-iprocorporate.png" alt="IproCorporate" title="" /></p>
+				</div>
+				<div class="col-xs-10">
+					<h3>IproCorporate (<em>2008 - 2009</em>)</h3>
+					<h4>Assistante de communication</h4>
+					<p>Organisation événementielle, conception de sites Internet, coordination de supports print et marketing</p>
+				</div>
+			</div><!-- Fin du Row IPROCORPORATE -->
+			
+
+
+		</div><!-- fin du container -->
+
+<?php include('inc/footer.php'); ?>
